c8cc265c1690cdace026a2265e576092ad57ad28
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-11-18 11:07:42 +0900

    search engine

---
+++ luha/modules/search_engine.py
@@ -0,0 +1,21 @@
+import sys
+
+QUERY_GOOGLE = '{0.author.mention} https://www.google.co.kr/search?q={1}'
+QUERY_NAVER = '{0.author.mention} https://search.naver.com/search.naver?query={1}'
+QUERY_DAUM = '{0.author.mention} https://search.daum.net/search?q={1}'
+
+
+class SearchEngine(object):
+    def __init__(self):
+        self.queries = {'구글': QUERY_GOOGLE,
+                        '네이버': QUERY_NAVER,
+                        '다음': QUERY_DAUM}
+
+    def search(self, ctx, args=()):
+        if len(args) <= 1:
+            return '{0.author.mention} 검색엔진 검색어 형태로 입력해주세요.'.format(ctx)
+        if args[0] not in self.queries.keys():
+            return '{0.author.mention} 지원하지 않는 검색엔진입니다.'.format(ctx)
+        query_form = self.queries[args[0]]
+        query = ' '.join(args[1:])
+        return query_form.format(ctx, query)
---
+++ luha/tests/search_engine_tests.py
@@ -0,0 +1,33 @@
+import unittest
+import unittest.mock
+
+from luha.modules.search_engine import SearchEngine
+
+
+class SearchEngineTestCase(unittest.TestCase):
+    def setUp(self):
+        self.search = SearchEngine()
+
+    def tearDown(self):
+        del self.search
+
+    def test_no_args(self):
+        author_mock = unittest.mock.Mock(mention='@everyone')
+        ctx_mock = unittest.mock.Mock(author=author_mock)
+        args = ()
+        self.assertEqual('@everyone 검색엔진 검색어 형태로 입력해주세요.',
+                         self.search.search(ctx_mock, args))
+
+    def test_one_args(self):
+        author_mock = unittest.mock.Mock(mention='@everyone')
+        ctx_mock = unittest.mock.Mock(author=author_mock)
+        args = ('구글', '스플래툰2')
+        self.assertEqual('@everyone https://www.google.co.kr/search?q=스플래툰2',
+                         self.search.search(ctx_mock, args))
+
+    def test_two_args(self):
+        author_mock = unittest.mock.Mock(mention='@everyone')
+        ctx_mock = unittest.mock.Mock(author=author_mock)
+        args = ('구글', '스플래툰2', '아미보')
+        self.assertEqual('@everyone https://www.google.co.kr/search?q=스플래툰2 아미보',
+                         self.search.search(ctx_mock, args))
