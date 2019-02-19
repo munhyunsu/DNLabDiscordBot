1bd635f2fc9a9f2f39f52dc65c4d9e55b8e5cc4f
munhyunsu <munhyunsu@gmail.com>
2019-02-19 16:29:03 +0900

    Tarot

--- luha_bot/luha_commands.py
+++ luha_bot/luha_commands.py
@@ -1,12 +1,12 @@
 from discord.ext import commands
 
-from greeter import Greeter
-from item_selector import ItemSelector
-from kawibawibo import KawiBawiBo
-from dice import DiceRoll
-from search_engine import SearchEngine
-from deep_rock_paper_scissors import RockPaperScissors
-
+from luha_bot.greeter import Greeter
+from luha_bot.item_selector import ItemSelector
+from luha_bot.kawibawibo import KawiBawiBo
+from luha_bot.dice import DiceRoll
+from luha_bot.search_engine import SearchEngine
+from luha_bot.deep_rock_paper_scissors import RockPaperScissors
+from luha_bot.tarot import TarotReader
 
 class LuHaCommands(object):
     def __init__(self):
@@ -16,48 +16,49 @@ class LuHaCommands(object):
         self.dice = DiceRoll()
         self.search_engine = SearchEngine()
         self.dl_rps = RockPaperScissors('dl_rps_log.csv', 'users.json')
+        self.tarot_reader = TarotReader('img')
 
     @commands.command(name='hello',
                       aliases=['인사'])
     async def hello(self, ctx, *args):
-        """채널에 있는 사람에게 인사를 합니다.
-        사용법: 인사"""
+        """사용법: 인사"""
         await ctx.send(self.greeter.get_hello(ctx, args))
 
     @commands.command(name='select',
                       aliases=['선택', '랜덤'])
     async def select(self, ctx, *args):
-        """주어진 항목 중에서 무작위로 1개 선택합니다.
-        사용법: 선택 하나 둘 셋"""
+        """사용법: 선택 하나 둘 셋"""
         await ctx.send(self.selector.get_random(ctx, args))
 
     @commands.command(name='kbb',
                       aliases=['가위바위보'])
     async def kbb(self, ctx, *args):
-        """가위 바위 보를 합니다.
-        사용법: 가위바위보 가위 -> 가위바위보 결과"""
+        """사용법: 가위바위보 가위 -> 가위바위보 결과"""
         await ctx.send(self.kbb_referee.kbb_game(ctx, args))
 
     @commands.command(name='dice',
                       aliases=['주사위'])
     async def roll_dice(self, ctx, *args):
-        """주사위를 굴립니다.
-        사용법: 주사위 횟수D면체"""
+        """사용법: 주사위 횟수D면체"""
         await ctx.send(self.dice.roll_dice(ctx, args))
 
     @commands.command(name='search',
                       aliases=['검색'])
     async def search(self, ctx, *args):
-        """검색어가 포함된 URL을 표시합니다.
-        사용법: 검색 [구글|네이버|다음] [검색어]"""
+        """사용법: 검색 [구글|네이버|다음] [검색어]"""
         await ctx.send(self.search_engine.search(ctx, args))
 
     @commands.command(name='rps',
                       aliases=['가위', '바위', '보'])
     async def rps(self, ctx, *args):
-        """딥러닝 가위바위보 봇과 대결합니다.
-        사용법: [가위|바위|보]"""
+        """사용법: [가위|바위|보]"""
         await ctx.send(self.dl_rps.do_battle(ctx, args))
+
+    @commands.command(name='fortune',
+                      aliases=['타로'])
+    async def fortune_tarot(self, ctx, *args):
+        """사용법: 타로"""
+        await self.tarot_reader.fortune(ctx, args)
 
 
 def setup(bot):
