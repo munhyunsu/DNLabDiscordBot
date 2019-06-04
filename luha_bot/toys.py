cd8902962afe3eab8bc8a5406bbc01556b4be607
munhyunsu <munhyunsu@gmail.com>
2019-06-04 12:49:54 +0900

    imgur

--- luha_bot/toys.py
+++ luha_bot/toys.py
@@ -13,4 +13,4 @@ class Toys(object):
         self.attendance.add(ctx.author.id)
         if len(self.attendance) > 2:
             self.attendance.clear()
-            return 'https://drive.google.com/uc?id=1eNXoIL8zST9Gg1DzTquI7Lh90ltuec3H'
+            return 'https://imgur.com/a/22Sk1HS'
