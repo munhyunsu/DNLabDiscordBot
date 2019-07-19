c7c21719f37a11d6058a26d5f5715c48d03d2aa4
munhyunsu <munhyunsu@gmail.com>
2019-07-19 13:50:43 +0900

    Foodbot

---
+++ luha_bot/cnu_food.py
@@ -0,0 +1,11 @@
+import 
+
+class CNUFoo(object):
+    def __init__(cache=''):
+        """
+        Load saved cache, ready to save data
+        """
+        self.cache_path = cache
+
+    def get_food():
+        
